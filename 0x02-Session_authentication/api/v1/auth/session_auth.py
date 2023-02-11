78bd086f8adf62eec93eb3b02092a2b3bef22b80
HAMA Barhamou <hamabarhamou@gmail.com>
2023-02-11 21:43:23 +0100

    1. Empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Session authentication"""
+
+from .auth import Auth
+from uuid import uuid4
+
+
+class SessionAuth(Auth):
+    """ Session Authentication
+    """
+    pass

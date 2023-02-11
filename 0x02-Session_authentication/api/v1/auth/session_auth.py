45494ed4238e1b00eada6de1056dbf43233e132f
HAMA Barhamou <hamabarhamou@gmail.com>
2023-02-11 23:39:01 +0100

    8. Logout pcodestyle

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -38,7 +38,7 @@ class SessionAuth(Auth):
         user_id = self.user_id_for_session_id(session_id)
 
         return User.get(user_id)
-    
+
     def destroy_session(self, request=None):
         """
         """
